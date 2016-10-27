86f93b951dbf30dd44a65c9703fc8896319711f5
André Brait <andrebrait@gmail.com>
2016-10-27 14:42:29 -0200

    Default config

--- omnisync.py
+++ omnisync.py
@@ -8,6 +8,16 @@ if __name__ == "__main__":
 
     db = dataset.connect("sqlite:////home/andre/.omnisync/database.db")
 
+    config = db['config']
+    down_limit = config.find_one(name='down_limit')
+    up_limit = config.find_one(name='up_limit')
+    config = config.find_one(name='max_conn')
+
+    if not down_limit or not up_limit or not config:
+        config.insert(dict(name='down_limit', value='0'))
+        config.insert(dict(name='up_limit', value='0'))
+        config.insert(dict(name='max_conn', value='4'))
+
     task_queue = TaskQueue()
     pool = Pool(task_queue)
     pool.set_worker_num(4)
